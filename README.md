b03f063076783b3f59296f590edcac5ea89c72c0
Ezerous <ezerous@gmail.com>
2019-02-09 20:43:29 +0200

    Added support for incognito mode

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # DarkTube
 
-Extension that forces YouTube to use dark theme.
+Firefox add-on that forces YouTube to use dark theme.
 Can be added to Firefox from [here](https://addons.mozilla.org/en-US/firefox/addon/darktube/).
-
-*Note: Does not work in incognito mode.*
--- background-script.js
+++ background-script.js
@@ -1,14 +1,20 @@
 var storedCookieValue;
 
 browser.cookies.onChanged.addListener(function(changeInfo) {
-  if(changeInfo.cookie.name === "PREF" && changeInfo.cookie.value !== storedCookieValue)
+  if(changeInfo.cookie.name == "PREF" && changeInfo.cookie.value != storedCookieValue)
     setCookie();
 });
+
+// For incognito mode
+browser.webRequest.onBeforeSendHeaders.addListener(setCookieHeaderIfMissing,
+                                          {urls: ["*://*.youtube.com/*"]},
+                                          ["blocking", "requestHeaders"]);
 
 function setCookie() {
   var cookies = browser.cookies.getAll({name:"PREF", domain: ".youtube.com"}).then(
     (cookies) => {
-      if(cookies[0])
+
+      if(cookies[0])  // Check if cookie exists
         storedCookieValue = patchValue(cookies[0].value);
       else
         storedCookieValue = "f6=400";
@@ -16,14 +22,27 @@ function setCookie() {
       browser.cookies.set({name: "PREF", url: "http://.youtube.com/", value: storedCookieValue});
     }
   );
-
-
 }
 
-function patchValue(value){
+// Patches cookie value for dark theme
+function patchValue(value) {
   var regex = /f6=(\d+)/;
   value = value.replace(regex, "");
   return value + "&f6=400"
 }
 
-setCookie();
+// Adds cookie in header if it doesn't exist (for incognito mode)
+function setCookieHeaderIfMissing(e) {
+  var found;
+  for (var header of e.requestHeaders) {
+    if (header.name.toLowerCase() === "cookie"){
+      found=true;
+      break;
+    }
+  }
+  if(!found)
+    e.requestHeaders.push({name: "Cookie", value: "PREF=f6=400;"});
+  return {requestHeaders: e.requestHeaders};
+}
+
+setCookie();  // Run once when add-on starts
