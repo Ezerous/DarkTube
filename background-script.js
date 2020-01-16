0ce147428dadabaa5defee031680a1826de43cc2
Ezerous <ezerous@gmail.com>
2020-01-16 13:02:15 +0200

    Small fix

--- background-script.js
+++ background-script.js
@@ -1,4 +1,4 @@
-var storedCookieValue;
+let storedCookieValue;
 
 browser.cookies.onChanged.addListener(function(changeInfo) {
   if(changeInfo.cookie.name == "PREF" && changeInfo.cookie.value != storedCookieValue)
@@ -11,14 +11,12 @@ browser.webRequest.onBeforeSendHeaders.addListener(setCookieHeaderIfMissing,
                                           ["blocking", "requestHeaders"]);
 
 function setCookie() {
-  var cookies = browser.cookies.getAll({name:"PREF", domain: ".youtube.com"}).then(
+  browser.cookies.getAll({name:"PREF", domain: ".youtube.com"}).then(
     (cookies) => {
-
       if(cookies[0])  // Check if cookie exists
         storedCookieValue = patchValue(cookies[0].value);
       else
         storedCookieValue = "f6=400";
-
       browser.cookies.set({name: "PREF", url: "http://.youtube.com/", value: storedCookieValue});
     }
   );
@@ -26,23 +24,22 @@ function setCookie() {
 
 // Patches cookie value for dark theme
 function patchValue(value) {
-  var regex = /f6=(\d+)/;
-  value = value.replace(regex, "");
-  return value + "&f6=400"
+  value = value.replace(/&?f6=\d+/, "");
+  if(!value)
+    return "f6=400";
+  return value + "&f6=400";
 }
 
 // Adds cookie in header if it doesn't exist (for incognito mode)
 function setCookieHeaderIfMissing(e) {
-  var found;
-  for (var header of e.requestHeaders) {
-    if (header.name.toLowerCase() === "cookie"){
-      found=true;
-      break;
-    }
-  }
-  if(!found)
-    e.requestHeaders.push({name: "Cookie", value: "PREF=f6=400;"});
-  return {requestHeaders: e.requestHeaders};
+  let headers = e.requestHeaders;
+  const index = headers.findIndex((header) => (header.name.toLowerCase() === "cookie"));
+
+  if(index>=0)
+   headers[index]={name: "Cookie", value: patchValue(headers[index].value)};
+  else
+    headers.push({name: "Cookie", value: "PREF=f6=400;"});
+  return {requestHeaders: headers};
 }
 
 setCookie();  // Run once when add-on starts
