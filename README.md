c81defa7d36c9a5a2795b02f635047a4df705a4c
Ezerous <ezerous@gmail.com>
2020-02-03 20:26:35 +0200

    Major bug fixes & optimizations

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 [![Version](https://img.shields.io/amo/v/darktube.svg)](https://addons.mozilla.org/en-US/firefox/addon/darktube/)
 
-Firefox extension that forces YouTube to use dark theme (even in private windows, if you enable it).
-Can be added to Firefox from [here](https://addons.mozilla.org/en-US/firefox/addon/darktube/).
+Firefox extension that forces YouTube to use dark theme.
+It removes some minor annoyances as well (e.g. default autoplay when not set by the user and a few annoying tooltips/banners).
+Also works in private windows (if allowed).
--- background.js
+++ background.js
@@ -1,36 +1,39 @@
-let storedCookieValue;
-
 browser.cookies.onChanged.addListener(function(changeInfo) {
-  if(changeInfo.cookie.name == "PREF" && changeInfo.cookie.value != storedCookieValue)
-    setCookie();
+  if(changeInfo.cookie.name == "PREF")
+    setCookie(changeInfo.cookie.storeId);
 });
 
-function setCookie() {
-  browser.cookies.getAllCookieStores().then(
-    (cookieStores) => {
-      for (let store of cookieStores){
-        browser.cookies.getAll({name:"PREF", domain: ".youtube.com"}).then(
-          (cookies) => {
-            if(cookies[0])  // Check if cookie exists
-              storedCookieValue = patchCookieValue(cookies[0].value);
-            else
-              storedCookieValue = "f6=400";
-            browser.cookies.set({name: "PREF", url: "http://.youtube.com/", value: storedCookieValue, storeId: store.id});
-          }
-        );
-      }
+function setCookie(storeId) {
+  browser.cookies.getAll({name:"PREF", domain:".youtube.com", storeId:storeId}).then(
+    (cookies) => {
+      if(cookies[0])  // Check if cookie exists
+        patchedCookieValue = patchCookieValue(cookies[0].value);
+      else
+        patchedCookieValue = "f6=400";
+      browser.cookies.set({name:"PREF", url: "http://.youtube.com/", value:patchedCookieValue, storeId:storeId});
     }
   );
 }
 
-// Patches cookie value for dark theme
+// Patches cookie value for dark theme (f6) and disables autoplay if it is not set by the user (f5)
 function patchCookieValue(value) {
   value = value.replace(/&?f6=\d+/, "");
   if(!value)
-    return "f6=400";
-  if(value.startsWith('&'))
-    value = value.substr(1);
-  return value + "&f6=400";
+    value = "f6=400&f5=30000";
+  else {
+    if(value.startsWith('&'))
+      value = value.substr(1);
+    value = value + "&f6=400";
+    if(!value.includes("f5"))  //User has made his own choice about autoplay
+      value = value + "&f5=30000";
+  }
+  return value;
 }
 
-setCookie();  // Run once when add-on starts
+// Run once when add-on starts
+browser.cookies.getAllCookieStores().then(
+  (cookieStores) => {
+    for (let store of cookieStores)
+      setCookie(store.id);
+  }
+);
